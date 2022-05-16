efd942d1a45d7d6d01d9517bc91851449f0765fc
angelmizutani <angelisayuri@yahoo.com.br>
2022-05-16 20:58:32 -0300

    aula16mai2022

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Index
+from .views import Index, CidadeCreate, PessoaCreate
 
 urlpatterns = [
     # path('inicio/', Index.as_view(), name='index'),
     path('', Index.as_view(), name='index'),
+    path('cadastrar/cidade/', CidadeCreate.as_view(), name="cadastrar-cidade"),
+    path('cadastrar/pessoa/', PessoaCreate.as_view(), name="cadastrar-pessoa"),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,4 +1,20 @@
 from django.views.generic import TemplateView
+from .models import Cidade, Pessoa
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
 class Index(TemplateView):
     template_name = 'paginas/index.html'
+
+class CidadeCreate(CreateView):
+    model = Cidade
+    fields = ['nome', 'estado']
+    template_name = 'cadastros/formulario.html'
+    success_url = reverse_lazy('index')
+
+class PessoaCreate(CreateView):
+    model = Pessoa
+    fields = ['nome', 'nascimento', 'email', 'cidade']
+    template_name = 'cadastros/formulario.html'
+    success_url = reverse_lazy('index')
+ 
